da39c380743769964dd30a54eb8b05ad92f79c54
Frits Kuipers <frits.kuipers@gmail.com>
2017-02-21 14:49:49 +0000

    Cleanup

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-### Telegram to Mumble bridge using Espeak.
+### Telegram to Mumble bridge using Espeak in Node.js
+
+Dependencies:
+
+    npm i mumble node-telegram-bot-api node-espeak node-yaml-config
 
 To generate certificates:
 
     openssl req -x509 -newkey rsa:2048 -nodes -keyout key.pem -out cert.pem
+
+Run with:
+
+    node mumbletg.js
--- mumbletg.js
+++ mumbletg.js
@@ -2,8 +2,9 @@
  * Telegram to mumble bridge using Espeak.
  */
 
-var mumble = require('mumble'), fs = require('fs');
-var Tg = require('node-telegram-bot-api');
+var mumble = require('mumble'),
+    fs = require('fs');
+var tg = require('node-telegram-bot-api');
 var ESpeak = require('node-espeak');
 var yaml_config = require('node-yaml-config');
 
@@ -14,54 +15,57 @@ var config = yaml_config.load('config.yml');
 var stream;
 
 var options = {
-    key: fs.readFileSync( 'key.pem' ),
-    cert: fs.readFileSync( 'cert.pem' ),
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
 };
 
 console.log("Initialize Telegram Bot.")
-const bot = new Tg(config.telegram_token, {polling: true});
-
-console.log( 'Connecting to: ' + config.server );
-mumble.connect( config.server, options, function ( error, connection ) {
-    if( error ) { throw new Error( error ); }
-
-    console.log( 'Connected' );
-
-    connection.authenticate( config.name );
-    connection.on( 'initialized', function() {
-        stream = connection.inputStream({channels: 1, sampleRate: 22100, gain: 1});
-        generateSpeech(connection);
-    });
-    connection.on( 'voice', onVoice );
+const bot = new tg(config.telegram_token, {
+    polling: true
 });
 
-var onVoice = function( voice ) {
-};
+console.log('Connecting to: ' + config.server);
+mumble.connect(config.server, options, function(error, connection) {
+    if (error) {
+        throw new Error(error);
+    }
 
-var generateSpeech = function(client) {
+    console.log('Connected');
+    connection.authenticate(config.name);
+    connection.on('initialized', () => generateSpeech(connection));
+});
+
+/**
+ * Initializes ESpeak and registers its callback.
+ *
+ * @param      connection  The connection with mumble.
+ */
+var generateSpeech = function(connection) {
+    stream = connection.inputStream({
+        channels: 1,
+        sampleRate: 22100,
+        gain: 1
+    });
+
     ESpeak.initialize();
 
-    ESpeak.setLanguage( config.espeak.language );
-    ESpeak.setGender( config.espeak.gender );
-    ESpeak.setPitch( config.espeak.pitch );
-    ESpeak.setRange( config.espeak.range );
-    ESpeak.setGap( config.espeak.gap );
-    ESpeak.setRate( config.espeak.rate );
-    ESpeak.setVolume( config.espeak.volume );
+    ESpeak.setLanguage(config.espeak.language);
+    ESpeak.setGender(config.espeak.gender);
+    ESpeak.setPitch(config.espeak.pitch);
+    ESpeak.setRange(config.espeak.range);
+    ESpeak.setGap(config.espeak.gap);
+    ESpeak.setRate(config.espeak.rate);
+    ESpeak.setVolume(config.espeak.volume);
 
     ESpeak.onVoice(function(wav, samples, samplerate) {
-        stream.write( wav );   
+        stream.write(wav);
     });
 
     ESpeak.speak(config.introduction);
 };
 
 bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  var text = msg.from.first_name + " " + msg.text;
-
-  ESpeak.speak(text);
-
-  console.log(msg);
+    var text = msg.from.first_name + ": " + msg.text;
+    ESpeak.speak(text);
+    console.log(text);
 });
